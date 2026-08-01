c81aeb8b4cec9a6d21ec71241a3a36400c41f792
Sharbel Zarzour <sharbel343@gmail.com>
2026-08-01 21:17:57 +0300

    Initial commit

---
+++ src/main/java/org/example/NodeInfo.java
@@ -0,0 +1,36 @@
+package org.example;
+
+@SuppressWarnings("unused")
+public class NodeInfo {
+    private int num;
+    private String info;
+
+    public NodeInfo(int num, String info) {
+        this.num = num;
+        this.info = info;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+
+    @Override
+    public String toString() {
+        return "NodeInfo{" +
+                "num=" + num +
+                ", info='" + info + '\'' +
+                '}';
+    }
+}
